8a3a2da262c5a370b1764ede7ad85e4dffc3851f
mhemu95 <mhemu95@gmail.com>
2021-09-05 00:40:21 +0600

    changed homepage and detailpage

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('create/', views.create_post, name='create_post'),
+    path('', views.home, name='post_home'),
     path('post/<id>/', views.post_detail, name='post_detail'),
-    path('update/<id>/', views.update_post, name='update_post'),
-    path('delete/<id>/', views.delete_post, name='delete_post'),
+    #crud urls..
+    path('panel/', views.post_list, name='post_list'),
+    path('panel/create/', views.create_post, name='create_post'),
+    path('panel/update/<id>/', views.update_post, name='update_post'),
+    path('panel/delete/<id>/', views.delete_post, name='delete_post'),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -3,20 +3,28 @@ from .models import Post
 from .forms import Postform
 
 
+def home(request):
+    blogs = Post.objects.all()
+    context = {
+        'blogs' : blogs
+    }
+    return render(request, 'front/home.html', context)
+
 def post_list(request):
     posts = Post.objects.all()
     context = {
         'post_list' : posts
     }
-    return render(request, 'post_list.html', context)
+    return render(request, 'back/post_list.html', context)
 
+#CRUD starts here...
 def post_detail(request, id):
     # In your detail template, you don't need a for loop; As you are just passing one post to the template:
     post = Post.objects.get(id=id)
     context = {
         'post': post
     }
-    return render(request, 'post_detail.html', context)
+    return render(request, 'front/post_detail.html', context)
 
 def create_post(request):
     form = Postform()
@@ -29,7 +37,7 @@ def create_post(request):
         'form': form,
         'type': 'Create'
     }
-    return render(request, 'create_post.html', context)
+    return render(request, 'back/create_post.html', context)
 
 def update_post(request, id):
     post = Post.objects.get(id=id)
@@ -43,7 +51,7 @@ def update_post(request, id):
         'form': form,
         'type': 'Update' 
     }
-    return render(request, 'create_post.html', context)
+    return render(request, 'back/create_post.html', context)
 
 def delete_post(request, id):
     post = Post.objects.get(id=id)
@@ -53,4 +61,4 @@ def delete_post(request, id):
     context = {
         'post' : post
     }
-    return render(request, 'delete_post.html', context)
+    return render(request, 'back/delete_post.html', context)
